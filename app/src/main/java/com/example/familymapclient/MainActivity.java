8b48cf2b3ded2820551f189cce07d189b5f7ba4d
Rebekah Daniels <math919191@gmail.com>
2023-03-14 21:17:21 -0600

    added jar from server

--- app/src/main/java/com/example/familymapclient/MainActivity.java
+++ app/src/main/java/com/example/familymapclient/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import Request.UserLoginRequest;
 
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
